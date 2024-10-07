2b6b1117181941c538b4f1ce1a18fab3c70bd4f6
Tofu <100872875+Tofu1004@users.noreply.github.com>
2024-10-06 21:44:09 -0400

    Added new js project

---
+++ javascript_projects/dog_years.js
@@ -0,0 +1,23 @@
+//setting a variable to my age
+let myAge = 30;
+
+//setting a numerical variable that can be changed
+let earlyYears = 2;
+earlyYears *= 10.5;
+
+//setting a variable that is my age - 2
+var laterYears = myAge -= 2;
+
+//multiplying my later years by 4 to get # of dog years
+laterYears *= 4;
+
+//writing a variable for my age in dog years
+var myAgeInDogYears = earlyYears + laterYears
+
+//setting my name to all lowercase letters
+const myName = 'Tofu';
+console.log(myName.toLowerCase());
+
+//displaying my name, human age, and dog age
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`)
+
